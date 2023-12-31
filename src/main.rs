9a1f9fa6f3569156f0b953de21fd2a334b222e65
abishevs <abisevs.eduards@gmail.com>
2023-12-31 21:13:32 +0100

    feat: shapes. 42

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,15 @@ use std::f64::consts;
 const PI: f64 = consts::PI;
 
 #[derive(Debug)]
+enum Shape {
+    Circle,
+    Rectangle,
+}
+
+#[derive(Debug)]
 struct Object {
     mass: Mass,
+    shape: Shape,
 }
 
 #[derive(Debug)]
@@ -49,12 +56,11 @@ impl Spring {
 }
 
 trait SimpleHarmonicMotion {
-    // add code here
     fn shm(&self, time: f64) -> f64;
 }
 
 impl SimpleHarmonicMotion for Spring {
-    // calculate x-positon given time T
+    // calculate kjr-positon given time T
     fn shm(&self, time: f64) -> f64 {
         let pos = self.motion.angular_velocity * time + self.motion.phase_angle;
         self.motion.amplitude * pos.sin()
@@ -63,7 +69,7 @@ impl SimpleHarmonicMotion for Spring {
 
 fn main() {
     let shm_params = SHMParams{angular_velocity: 1.0, amplitude: 5.0, phase_angle: 0.0};
-    let obj = Object { mass: Mass::Value(2.0)}; 
+    let obj = Object { mass: Mass::Value(2.0), shape:Shape::Rectangle}; 
     let test = Spring::new(1.9, 
                            2.9, 
                            Mass::MassLess, 
